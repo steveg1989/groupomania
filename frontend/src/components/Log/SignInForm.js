f5b8846dcfcc692e154d5c89ffb97fceb4ecf48f
steveg1989 <steveg1989@gmail.com>
2022-09-10 23:35:09 -0400

    add get user data

--- frontend/src/components/Log/SignInForm.js
+++ frontend/src/components/Log/SignInForm.js
@@ -1,59 +1,87 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import axios from "axios";
+import { UserContext } from "../AppContext";
+import { useNavigate } from "react-router-dom";
 
 const SignInForm = () => {
-	const [mail, setmail] = useState("");
-	const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	const handleLogin = (e) => {
-		e.preventDefault();
-		const error = document.querySelector(".error");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dataUserContext = useContext(UserContext);
 
-		if (mail.length < 24 && (mail.length > 6) & /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(mail)) {
-			error.innerHTML = "";
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const error = document.querySelector(".error");
+    if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)) {
+      error.innerHTML = "";
 
-			axios({
-				method: "post",
-				URL: `http://localhost:3001/api/user/login`,
-				withCredentials: true,
-				data: {
-					mail,
-					password,
-				},
-			})
-				.then((res) => {
-					if (res.data.error) {
-						error.innerHTML = res.data.message;
-					} else {
-						window.location = "/";
-					}
-				})
-				.catch((err) => {
-					console.log(err);
-				});
-		} else {
-			error.innerHTML = "Invalid Email";
-		}
-	};
+      axios({
+        method: "post",
+        url: `http://localhost:3001/api/user/login`,
+        withCredentials: true,
+        data: {
+          email,
+          password,
+        },
+      })
+        .then((res) => {
+          if (res.data.error) {
+            error.innerHTML = res.data.message;
+          } else {
+            // get user data
+            const data = res.data;
 
-	return (
-		<div>
-			<form action="" onSubmit={handleLogin} id="sign-up-form">
-				<label htmlFor="mail">email</label>
-				<br />
-				<input type="text" name="mail" id="mail" value={mail} onChange={(e) => setmail(e.target.value)} />
-				<br />
-				<br />
-				<br />
-				<label htmlFor="password">Password</label>
-				<br />
-				<input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-				<div className="error"></div>
-				<br />
-				<input type="submit" value="To log in" />
-			</form>
-		</div>
-	);
+            const dataProfile = {
+              email: data.email,
+              firstname: data.firstname,
+              lastname: data.lastname,
+              message: data.message,
+              userId: data.userId,
+            };
+            dataUserContext.updateUserdata(dataProfile);
+
+            navigate("/profile");
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    } else {
+      error.innerHTML = "Invalid Eemail";
+    }
+  };
+
+  return (
+    <div>
+      <form action="" onSubmit={handleLogin} id="sign-up-form">
+        <label htmlFor="email">email</label>
+        <br />
+        <input
+          type="text"
+          name="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br />
+        <br />
+        <br />
+        <label htmlFor="password">Password</label>
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <div className="error"></div>
+        <br />
+        <input type="submit" value="To log in" />
+      </form>
+    </div>
+  );
 };
 
 export default SignInForm;
