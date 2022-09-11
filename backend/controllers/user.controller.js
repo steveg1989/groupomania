215344527d9287e30878413e947f54b17643e1e9
steveg1989 <steveg1989@gmail.com>
2022-09-10 23:30:03 -0400

    add response data

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -5,21 +5,35 @@ const pipeline = promisify(require("stream").pipeline);
 
 // user info
 module.exports.userInfo = (req, res, next) => {
-	const { id: userId } = req.params;
-	const sqlRequest = `SELECT id, firstname, lastname, email, picture, isAdmin, isDeleted FROM user WHERE userid = ${userId};`;
+	const userId = req.userId;
+	const sqlRequest = `SELECT userId, firstname, imageurl, lastname, email FROM user WHERE userid = ${userId};`;
 
 	db.query(sqlRequest, (err, result) => {
 		if (err) {
 			res.status(404).json({ err });
 		}
-		res.status(200).json(result[0]);
+		
+		// response data
+		const user = {
+			email: result[0].email,
+			userId: result[0].userId,
+			firstname: result[0].firstname,
+			lastname: result[0].lastname,
+			img: result[0].imageurl,
+		};
+			res.status(200).json(user);
 	});
 };
 
 // update img
 module.exports.updateImgProfile = async (req, res) => {
 	try {
-		if (req.file.detectedMimeType != "image/jpg" && req.file.detectedMimeType != "image/png" && req.file.detectedMimeType != "image/jpeg") throw Error("invalid file");
+		if (
+			req.file.detectedMimeType != "image/jpg" && 
+			req.file.detectedMimeType != "image/png" && 
+			req.file.detectedMimeType != "image/jpeg"
+		) 
+			throw Error("invalid file");
 		if (req.file.size > 500000) throw Error("max size");
 	} catch (err) {
 		res.status(201).json({ err });
