973c5edae929be7cdc8f28b3fa423229fe3866eb
steveg1989 <steveg1989@gmail.com>
2022-09-10 23:40:13 -0400

    adjustments

--- frontend/src/components/Routes/index.js
+++ frontend/src/components/Routes/index.js
@@ -1,26 +1,59 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { UserContext } from "../components/AppContext";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
-import Home from "../../pages/Home";
-import Profile from "../../pages/Profile";
-import NotFound from "../../pages/NotFound";
-import Connection from "../../pages/Connection";
-import NavBar from "../NavBar";
+import Home from "../pages/Home";
+import Profile from "../pages/Profile";
+import NotFound from "../pages/NotFound";
+import Connection from "../pages/Connection";
+import NavBar from "../components/NavBar";
+import axios from "axios";
 
-const index = () => {
-	return (
-		<div>
-			<Router>
-				<NavBar />
-				<Routes>
-					<Route path="/" exact element={<Home />} />
-					<Route path="/connection" exact element={<Connection />} />
-					<Route path="/profile" exact element={<Profile />} />
-					<Route path="*" element={<NotFound />} />
-				</Routes>
-			</Router>
-		</div>
-	);
-};
+function AllRoutes() {
+  const dataUserContext = useContext(UserContext);
+  const navigate = useNavigate();
 
-export default index;
+  useEffect(() => {
+    const fetchToken = async () => {
+      await axios({
+        method: "get",
+        url: `http://localhost:3001/api/user/get-info`,
+        withCredentials: true,
+      })
+        .then((res) => {
+          if (res.status === 200) {
+            // get user data
+            const data = res.data;
+
+            const dataProfile = {
+              email: data.email,
+              firstname: data.firstname,
+              lastname: data.lastname,
+              message: data.message,
+              userId: data.userId,
+            };
+            dataUserContext.updateUserdata(dataProfile);
+            navigate("/profile");
+          } else {
+            navigate("/connection");
+          }
+        })
+        .catch((err) => console.log(err));
+    };
+    fetchToken();
+  }, []);
+
+  return (
+    <div className="app">
+      <NavBar />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/connection" exact element={<Connection />} />
+        <Route path="/profile" exact element={<Profile />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default AllRoutes;
