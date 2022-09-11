f65aa9f0b67b2d0ebee26d02c83f2f849f174413
steveg1989 <steveg1989@gmail.com>
2022-09-10 23:40:30 -0400

    adjustments

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,55 +5,45 @@ import { UserContext } from "./AppContext";
 import axios from "axios";
 
 const NavBar = () => {
-    const userId = useContext(UserContext);
-		const [userPic, setUserPic] = useState();
-		const [userFirstName, setUserFirstName] = useState();
-
-		useEffect(() => {
-			const getPicAndName = async () => {
-				await axios({
-					method: "get",
-					url: `${process.env.REACT_APP_API_URL}api/user/${userId}`,
-					withCredentials: true,
-			})
-					.then((res) => {
-									setUserFirstName(res.data.user_first_name);
-									setUserPic(res.data.user_picture);
-				})
-				.catch((err) => {
-							console.log(err);
-				});
-		};
-
-		getPicAndName();
-
-		if (userFirstName && userPic);
-	}, [userId, userFirstName, userPic]);
-
+  const dataUser = useContext(UserContext);
 
   return (
-		<div className="nav-container">
+    <div className="nav-container">
       <ul></ul>
       <NavLink to="/">
-			<img className="logo" src="./assets/logos/icon-left-font-monochrome-black.png" alt="logo" />
+        <img
+          className="logo"
+          src="./assets/logos/icon-left-font-monochrome-black.png"
+          alt="logo"
+        />
       </NavLink>
-      {userId ? (
-				<ul>
-					<li></li>
-					<li className="welcome">
-						<NavLink to="/profile">
-							
-              {userPic ? <img className="profile-pic" src={userPic} alt="profile-pic" /> : <img className= "profile-pic" src="./assets/img/default-profile.jpeg" alt="profile-pic" />}
-							<h5>{userFirstName}</h5>
-						</NavLink>
-					</li>
-					
-				</ul>
-			) : (
-				<ul></ul>
-			)}
-		</div>
-	);
+      {dataUser.dataProfile.userId ? (
+        <ul>
+          <li></li>
+          <li className="welcome">
+            <NavLink to="/profile">
+              {dataUser.dataProfile.img ? (
+                <img
+                  className="profile-pic"
+                  src={dataUser.dataProfile.img}
+                  alt="profile-pic"
+                />
+              ) : (
+                <img
+                  className="profile-pic"
+                  src="./assets/img/default-profile.jpg"
+                  alt="profile-pic"
+                />
+              )}
+              <h5>{dataUser.dataProfile.firstname}</h5>
+            </NavLink>
+          </li>
+        </ul>
+      ) : (
+        <ul></ul>
+      )}
+    </div>
+  );
 };
 
 export default NavBar;
