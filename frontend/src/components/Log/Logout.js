be6042152dc4738c0a80603b706a3d0612b7d4ac
Oreol Noumodong <oreolnoumodong@gmail.com>
2022-09-13 18:01:49 +0100

    fix redirect to login when user is no connected

--- frontend/src/components/Log/Logout.js
+++ frontend/src/components/Log/Logout.js
@@ -1,13 +1,7 @@
-import React, { useContext } from "react";
+import React from "react";
 import axios from "axios";
-import cookie from "js-cookie";
-import { useNavigate } from "react-router-dom";
-import { UserContext } from "../AppContext";
 
 const Logout = () => {
-  const navigate = useNavigate();
-  const dataUserContext = useContext(UserContext);
-
   const logout = async () => {
     await axios({
       method: "get",
