e110d33e89ed45b0120c0a5c8b3f4ed82690b74f
steveg1989 <steveg1989@gmail.com>
2022-09-14 13:40:48 -0400

    fix redirect to login when user is not connected

--- frontend/src/components/Log/Logout.js
+++ frontend/src/components/Log/Logout.js
@@ -1,29 +1,24 @@
 import React from "react";
 import axios from "axios";
-import cookie from "js-cookie";
 
 const Logout = () => {
-	const removeCookie = (key) => {
-		if (window !== "undefined") {
-			cookie.remove(key);
-		}
-	};
-
 	const logout = async () => {
 		await axios({
 			method: "get",
 			url: `${process.env.REACT_APP_API_URL}api/user/logout`,
 			withCredentials: true,
 		})
-			.then(() => removeCookie("jwt"))
-			.catch((err) => console.log(err));
+			.then((res) => {
+			if (res.status === 200) {
 		window.location = "/connection";
-	};
+	  	}
+		})
+		.catch((err) => console.log(err));
+  };
 
 	return (
 		<li onClick={logout} className= "logout-btn">
       <p>Sign out</p>
-			
 		</li>
 	);
 };
