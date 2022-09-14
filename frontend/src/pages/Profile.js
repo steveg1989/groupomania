c6a422aa3af1b046b61f8e0fd07cf97c3ccfcdd4
steveg1989 <steveg1989@gmail.com>
2022-09-14 12:59:52 -0400

    fix redirect to login when user is not connected

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -3,15 +3,18 @@ import { UserContext } from "../components/AppContext";
 import UpdateProfile from "../components/Profile/UpdateProfile";
 
 const Profile = () => {
-	const data = useContext(UserContext);
-	const userId = data.dataProfile.userId;
-	return (
-		<div>
-		<div className="profile-page">
-			<UpdateProfile />
-		</div>
-	</div>
-	);
+  const userId = useContext(UserContext).dataProfile.userId;
+  if (!userId) {
+    window.location = "/connection";
+  }
+
+  return (
+    <div>
+      <div className="profile-page">
+        <UpdateProfile />
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
