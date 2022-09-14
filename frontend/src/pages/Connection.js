8f31ed1525e87b0b636c46aaaaf7abfe598727c3
steveg1989 <steveg1989@gmail.com>
2022-09-14 12:55:55 -0400

    fix redirect to login when user is not connected

--- frontend/src/pages/Connection.js
+++ frontend/src/pages/Connection.js
@@ -1,10 +1,16 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { UserContext } from "../components/AppContext";
 import Log from "../components/Log";
 		
 const Connection = () => {
-	const userId = useContext(UserContext).dataProfile.userId;
-	return <div>{!userId ? <Log /> : <h1>Already Connected</h1>}</div>;
+  const navigate = useNavigate();
+  const userId = useContext(UserContext).dataProfile.userId;
+  if (!userId) {
+    return <Log />;
+  } else {
+    navigate("/profile");
+  }
 };
 
 export default Connection;
